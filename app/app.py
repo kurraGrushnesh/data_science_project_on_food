3921a72f9b9812ccc86bad11c857e51ac5649a95
KURRAGRUSHNESHURK23CS7105 <kurragrushnesh@karunya.edu.in>
2025-04-03 19:09:26 +0530

    commit succesful

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from absl import app
 
-
 @app.post("/generate_recipe")
 def generate_recipe(ingredients: list):
     recipe = model.predict(ingredients)
--- initialize_models.py
+++
@@ -1,7 +0,0 @@
-# initialize_models.py
-from src.train import RecipeRecommender
-
-if __name__ == "__main__":
-    print("Initializing models and data...")
-    recommender = RecipeRecommender()
-    print("Success! Models and data ready.")
